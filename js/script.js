52df15043cf78802fc379ee6c3afb8e72fc7c159
Javier Ibarra Letelier <ibarraletelier@gmail.com>
2025-01-14 13:15:08 +0100

    6

--- js/script.js
+++ js/script.js
@@ -22,44 +22,44 @@ function changeQuote() {
 
 // Variables para controlar el intervalo y el estado
 let interval;
-let isChanging = true; // Estado que indica si está cambiando la frase o no
+let isChanging = false; // Estado que indica si está cambiando la frase o no
 
 // Iniciar el cambio de frases cada 100ms
 function startChanging() {
-  if (!isChanging) return; // Si ya está detenido, no hace nada
+  if (isChanging) return; // Si ya está cambiando, no hace nada
+  isChanging = true;
   interval = setInterval(changeQuote, 100); // Cambiar cada 100ms
 }
 
 // Detener el cambio de frases
 function stopChanging() {
   clearInterval(interval);
+  isChanging = false;
 }
 
 // Mostrar video con un fade-in
 function showBloodVideo() {
   videoContainer.style.opacity = 1;
-  bloodVideo.play(); // Asegura que el video se esté reproduciendo
 }
 
 // Ocultar video con un fade-out
 function hideBloodVideo() {
   videoContainer.style.opacity = 0;
-  bloodVideo.pause(); // Pausa el video cuando desaparece
 }
 
 // Evento de inicio de presión (mousedown/touchstart)
 function startPress() {
   if (isChanging) {
+    // Si está cambiando, detener el cambio de frases y mostrar el video
     stopChanging();  // Detiene el cambio de frases cuando se hace clic o se toca
     showBloodVideo(); // Muestra el video con fade-in
     bloodVideo.currentTime = 0; // Reinicia el video para que comience desde el inicio
+    bloodVideo.play(); // Asegura que el video se esté reproduciendo desde el inicio
   } else {
+    // Si no está cambiando, reanudar el cambio de frases y ocultar el video
     startChanging();  // Reanuda el cambio de frases cuando se hace clic o se toca nuevamente
     hideBloodVideo(); // Oculta el video con fade-out
   }
-
-  // Alternar el estado de isChanging para controlar la animación de las frases
-  isChanging = !isChanging;
 }
 
 // Iniciar el cambio de frases al cargar la página
